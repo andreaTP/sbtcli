135bb5d7b417b65d1478e244709b8c0099a782a4
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2018-10-26 16:55:00 +0100

    minor

--- travis/setNodeVersion.sh
+++ travis/setNodeVersion.sh
@@ -6,5 +6,5 @@ if [ ! -z "$TRAVIS_NODE_VERSION" ]; then
   (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
   source ~/.nvm/nvm.sh
   nvm install $TRAVIS_NODE_VERSION
-  npm install
+  # npm install
 fi;
